871df310dc74d6003d118861d8e68a2e2ac6db15
Ru1n00 <ruhul.amin.tnt@gmail.com>
2023-08-18 20:17:36 +0600

    Complete all functionality

--- main.js
+++ main.js
@@ -9,10 +9,19 @@ fetch(weatherApi)
     .then(response => response.json())
     .then(data => {
         weatherReport = data;
-        console.log(weatherReport);
         displayWeather();
     });
 
+let tableElement = document.getElementsByClassName("table")[0];
+let filePath = "Final Air Quality Dataset.csv";
+xmlhttp = new XMLHttpRequest();
+xmlhttp.open("GET",filePath,false);
+xmlhttp.overrideMimeType('text/plain');
+xmlhttp.send(null);
+//maybe check status !=404 here
+let fileContent = xmlhttp.responseText;
+let fileArray = fileContent.split('\n')
+let n = fileArray.length;
 
 function displayWeather() {
     // variables
@@ -54,5 +63,86 @@ function displayWeather() {
     pressureElement.innerHTML = pressure + " mb";
     precipitationElement.innerHTML = precipitation + " mm";
 
-    console.log(weatherIconElement);
 };
+
+
+function generateTable() {
+    console.log(n, fileArray);
+
+    for(let i=0; i <= 50; i++) {
+        let row = fileArray[i].split(',');
+        if(i != 0) {
+            tableElement.innerHTML += `
+                <div class="row text-center fw-bold row-${row[0]}" role="button" style="font-size: 1rem;">
+                    <div class="col border-end border-bottom border-light">${row[1]}</div>
+                    <div class="col border-end border-bottom border-light">${row[2]}</div>
+                    <div class="col border-end border-bottom border-light">${row[3]}</div>
+                    <div class="col border-end border-bottom border-light">${row[4]}</div>
+                    <div class="col border-end border-bottom border-light">${row[5]}</div>
+                    <div class="col border-bottom border-light">${row[6]}</div>
+                </div>`;
+        } else {
+            tableElement.innerHTML += `
+                <div class="row text-center fw-bold row-${row[0]}" style="font-size: 1.1rem;">
+                    <div class="col border-end border-bottom border-light">${row[1]}</div>
+                    <div class="col border-end border-bottom border-light">${row[2]}</div>
+                    <div class="col border-end border-bottom border-light">${row[3]}</div>
+                    <div class="col border-end border-bottom border-light">${row[4]}</div>
+                    <div class="col border-end border-bottom border-light">${row[5]}</div>
+                    <div class="col border-bottom border-light">${row[6]}</div>
+                </div>`;
+        }
+    }
+
+    for(let i = 1; i <= 50; i++) {
+        let rowElement = document.getElementsByClassName(`row-${i}`)[0];
+        rowElement.addEventListener("click", () => {
+            handleRowClick(i);
+        });
+    }
+};
+
+function handleRowClick(i) {
+    let rowElement = document.getElementsByClassName(`row-${i}`)[0];
+    rowElement.classList.toggle("selected");
+    removeSelections(i);
+    let o3Element = document.getElementsByClassName("o3")[0];
+    let no2Element = document.getElementsByClassName("no2")[0];
+    let so2Element = document.getElementsByClassName("so2")[0];
+    let coElement = document.getElementsByClassName("co")[0];
+    let pm25Element = document.getElementsByClassName("pm25")[0];
+    let pm10Element = document.getElementsByClassName("pm10")[0];
+    let polutionStatusElement = document.getElementsByClassName("polution-status")[0];
+
+    let row = fileArray[i].split(',');
+    o3Element.innerHTML = row[1];
+    coElement.innerHTML = row[2];
+    so2Element.innerHTML = row[3];
+    no2Element.innerHTML = row[4];
+    pm25Element.innerHTML = row[5];
+    pm10Element.innerHTML = row[6];
+    if(row[7] === '1') {
+        polutionStatusElement.innerHTML = "Yes";
+        polutionStatusElement.style.color = "red";
+        polutionStatusElement.classList.remove("light-green");
+        polutionStatusElement.classList.add("border-danger");
+    } else {
+        polutionStatusElement.innerHTML = "No";
+        polutionStatusElement.style.color = "#76ff03";
+        polutionStatusElement.classList.remove("border-danger");
+        polutionStatusElement.classList.add("light-green");
+    }
+
+}
+
+function removeSelections(i) {
+    let tempo = "";
+    for(let j = 1; j <= 50; j++) {
+        if(j != i) {
+            tempo = document.getElementsByClassName(`row-${j}`)[0];
+            tempo.classList.remove("selected");
+        }
+    }
+}
+
+generateTable();
